e8f249e49707f83bcfccd123fb67ecf740c90ebb
Ryan Bigg <git@ryanbigg.com>
2015-12-15 09:30:46 +1100

    Add direction to spawn test

--- test/spawning_test.exs
+++ test/spawning_test.exs
@@ -1,7 +1,7 @@
 defmodule Ebot.BotTest do
   use ExUnit.Case
 
-  test "spawns at 0,0" do
-    assert Ebot.Bot.spawn == {0 , 0}
+  test "spawns at 0,0 and faces east" do
+    assert Ebot.Bot.spawn == {0 , 0, :east}
   end
 end
